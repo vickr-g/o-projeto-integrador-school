05c60376d3d6b42f34d68a0d8f51949d48b0d9b7
Vick Gindre <vgindre45@gmail.com>
2023-03-24 16:52:57 -0300

    iniciando a função do curso

---
+++ modulo - json/function.js
@@ -0,0 +1,11 @@
+// objetivo: fazer funcionar o projeto :)
+const cursosArquivo = require('./cursos.js')
+function getCursos(cusros){
+ let arrayDosCursos = cusros
+ let dadosCurso = []
+ let status = false
+
+ getCursos.cusros['cusros'].forEach(function(alunos) {
+     
+ });
+}
